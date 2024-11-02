358b860a3467773540326516716eaf4bd394fd80
Sateesh2004 <kumarsateesh838@gmail.com>
2024-11-02 06:59:03 +0530

    "GF"

--- frontend/src/app/(pages)/feedback/[uuid]/page.jsx
+++ frontend/src/app/(pages)/feedback/[uuid]/page.jsx
@@ -17,7 +17,7 @@ const Page = () => {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await fetch("http://localhost:3001/interview/showdetails", {
+                const response = await fetch("https://intervue2-wgit3nni.b4a.run/interview/showdetails", {
                     method: "POST",
                     headers: { "Content-Type": "application/json" },
                     credentials: 'include',

fe9300956fb2cdaf852f352d5a0c68db4db28a14
Sateesh2004 <kumarsateesh838@gmail.com>
2024-11-03 08:17:43 +0530

    "Logo Changed"

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer className="bg-white p-10 font-[sans-serif] tracking-wide">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
         <div className="lg:flex lg:items-center">
-          <div className=" text-black font-bold text-[30px]">Inter<span className='text-indigo-700'>Vue</span></div>
+          <div className=" text-black font-bold text-[30px]">Inter<span className='text-indigo-700'>Veu</span></div>
          
         </div>
 

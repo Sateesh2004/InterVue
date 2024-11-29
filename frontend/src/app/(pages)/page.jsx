3f6c48d87d4faf72ae70da2c1d9c607a967b0352
Sateesh2004 <kumarsateesh838@gmail.com>
2024-11-29 06:48:47 +0530

    "Homepage metadata changed"

--- frontend/src/app/(pages)/page.jsx
+++ frontend/src/app/(pages)/page.jsx
@@ -14,6 +14,20 @@ const outfit = Outfit({
 
 
 
+export const metadata = {
+  
+  title: {
+    template:
+      "Interveu • AI Powered Interview Preparation Tool",
+    default:
+      "Interveu • AI Powered Interview Preparation Tool",
+  },
+  description:
+    "At Interveu, we revolutionize the way you prepare for interviews. Our platform uses advanced AI to analyze your job descriptions and generate personalized interview for you.",
+    icons: {
+      icon: "/assets/favicon.png",
+    },
+};
 
 const page = () => {
   return (
